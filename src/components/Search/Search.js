b6057b2f541eda2ee8e3a9df63b190fe3755a130
Davit Vardanyan <jedavard@gmail.com>
2020-05-15 17:01:04 +0400

    search ui  improve

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,32 +7,43 @@ function Search() {
     return (
         <section className={classes.Search}>
             <div className="row">
-                <div className={classes.Search__content}>
-                    <form className={classes.Search__form}>
-                        <div className={classes.Search__inputs}>
-                            <div className={classes.Search__inputbox}>
-                                <label htmlFor="location">
-                                    <div>Location</div>
-                                <input type="text" name="location" placeholder={'Where to find?'} />
-                                </label>
+                <div>
+                    <form action="">
+                        <div className={classes.Search__form}>
+                            <div className={classes.Search__search}>
+                                <div className={classes.Search__cell}>
+                                    <div>
+                                        <div className={classes.Search__q}>
+                                            <label htmlFor="search" className={classes.Search__loc}>
+                                                <div className={classes.Search__loc__name}>Location</div>
+                                                <input type="text" className={classes.Search__loc__input}
+                                                       placeholder="Where to find?" id="search" />
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={classes.Search__sep}> </div>
+                                <div className={classes.Search__cell}>
+                                    <div className={classes.Search__q}>
+                                        <button className={classes.Search__buttonBox}>
+                                            <div className={classes.Search__buttonBox__title}>Tour start / end</div>
+                                            <div className={classes.Search__buttonBox__selection}>Add dates</div>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className={classes.Search__sep}> </div>
+                                <div className={classes.Search__cell}>
+                                    <div className={classes.Search__q}>
+                                        <button className={classes.Search__buttonBox}>
+                                            <div className={classes.Search__buttonBox__title}>Tour start / end</div>
+                                            <div className={classes.Search__buttonBox__selection}>Add dates</div>
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
-                            <Separator vertical margin={'-.1 0'} color={'middle'} height={'4.4'}/>
-                            <div className={classes.Search__input}>
-                                <button>
-                                    <div>Tour Start/End</div>
-                                    <div>Add Dates</div>
-                                </button>
+                            <div className={classes.Search__but}>
+                                <StyledButton>Search</StyledButton>
                             </div>
-                            <Separator vertical margin={'-.1 0'} color={'middle'} height={'4.4'}/>
-                            <div className={classes.Search__input}>
-                                <button>
-                                    <div>Participants</div>
-                                    <div>Add participants</div>
-                                </button>
-                            </div>
-                        </div>
-                        <div className={classes.Search__button}>
-                            <StyledButton>Search</StyledButton>
                         </div>
                     </form>
                 </div>
