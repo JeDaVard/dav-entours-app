d03629146af855dd8d1e87801f47a8020ca17a12
Davit Vardanyan <jedavard@gmail.com>
2020-05-25 02:50:13 +0400

    minor fix

--- src/OnlyAuth/OnlyAuth.js
+++ src/OnlyAuth/OnlyAuth.js
@@ -1,7 +1,6 @@
 import React from "react";
 import classes from './OnlyAuth.module.css';
-import img from './i.jpg'
-import i from './a.jpg'
+import img from './a.jpg';
 import StyledButton from "../components/StyledButton/StyledButton";
 import {Link} from "react-router-dom";
 import Separator from "../components/UI/Separator/Separator";
@@ -14,7 +13,7 @@ function OnlyAuth(props) {
                     <h2 className={classes.OnlyAuth__title}>Sign Up</h2>
                     <Separator margin={'2 2'} color={'normal'} />
                     <div className={classes.OnlyAuth__content}>
-                        <img src={i} alt="" className={classes.OnlyAuth__illustration} />
+                        <img src={img} alt="" className={classes.OnlyAuth__illustration} />
                         <StyledButton onClick={props.signUp}>Continue</StyledButton>
                         <div>
                             <h2 className={classes.OnlyAuth__switchText}>Already have account?</h2> <Link to={loc => loc} onClick={props.login}>Login</Link>
