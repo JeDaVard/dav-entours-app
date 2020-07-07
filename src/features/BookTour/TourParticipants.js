f4bc98098c9c781c2b22e4e7d914ba6b574869ef
Davit Vardanyan <jedavard@gmail.com>
2020-07-08 03:57:34 +0400

    booking invite friends feature

--- src/features/BookTour/TourParticipants.js
+++ src/features/BookTour/TourParticipants.js
@@ -3,8 +3,9 @@ import classes from "./TourParticipants.module.css";
 import SmallShow from "../../components/UI/SmallShow/SmallShow";
 import Justicon from "../../components/UI/JustIcon/Justicon";
 import Separator from "../../components/UI/Separator/Separator";
-import {Form, Input} from "../../components/UI/LabeledInput/LabeledInput";
+import {Form, Input, MultiInput} from "../../components/UI/LabeledInput/LabeledInput";
 import ShowAllMembers from "../TourContainer/TourOrder/ShowAllMembers";
+import SimpleButton from "../../components/UI/SimpleButton/SimpleButton";
 
 export default function (props) {
     const { me, start } = props;
@@ -25,18 +26,25 @@ export default function (props) {
                         <div className={classes.inviteHead}>
                             <h2>Invite a member</h2>
                         </div>
-                        <Separator color={'normal'} />
+                        <Separator color={'normal'} margin={'0 2'} />
                         <Form>
-                            <Input
-                                type='email'
-                                name="guestEmail"
-                                label="E-mail"
-                                id="inviteGuestEmail"
-                                // value={state.summary}
-                                // onChange={onInputChange}
-                                inputDescription="Come with your friend or family member,
+                            <MultiInput>
+                                <Input
+                                    type='email'
+                                    name="guestEmail"
+                                    label="E-mail"
+                                    id="inviteGuestEmail"
+                                    // value={state.summary}
+                                    // onChange={onInputChange}
+                                    inputDescription="Come with your friend or family member,
                                                     just add their account and pay"
-                            />
+                                />
+                                <button className={classes.add}>
+                                    <Justicon
+                                        className={classes.inviteIcon}
+                                        icon={'plus'} />
+                                </button>
+                            </MultiInput>
                         </Form>
                     </div>
                 </SmallShow>
