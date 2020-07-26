12068263f96515ed603a8e340c818a868b808214
Davit Vardanyan <jedavard@gmail.com>
2020-07-26 10:48:56 +0400

    apple pay

--- src/features/Payment/PaymentForm.js
+++ src/features/Payment/PaymentForm.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useLocation } from 'react-router-dom';
 import qs from "query-string";
 import classes from "./PaymentForm.module.css";
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { CardElement, useElements, useStripe, PaymentRequestButtonElement } from "@stripe/react-stripe-js";
 import StyledButton from "../../components/UI/StyledButton/StyledButton";
 import {useMutation} from "@apollo/client";
 import {INTENT_PAYMENT} from "./queries";
@@ -12,70 +12,97 @@ import './_payments.css'
 import Separator from "../../components/UI/Separator/Separator";
 
 export default function PaymentForm() {
-    const location = useLocation()
-    const { tourId, start, invite } = qs.parse(location.search);
-    const { message, me } = location.state;
+    // const location = useLocation()
+    // const { tourId, start, invite } = qs.parse(location.search);
+    // const { message, me } = location.state;
+
+    const [paymentRequest, setPaymentRequest] = useState(null);
 
     const stripe = useStripe();
-    const elements = useElements();
-    const [ intentPayment ] = useMutation(INTENT_PAYMENT, {
-        variables: {
-            tourId,
-            startId: start,
-            firstMessage: message,
-            invitedIds: invite
-        },
-    });
+    // const elements = useElements();
+    // const [ intentPayment ] = useMutation(INTENT_PAYMENT, {
+    //     variables: {
+    //         tourId,
+    //         startId: start,
+    //         firstMessage: message,
+    //         invitedIds: invite
+    //     },
+    // });
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        if (!stripe || !elements) return;
-
-        const response = await intentPayment();
-        console.log(response)
-        const { clientSecret } = response.data.intentTourPayment;
-
-        const result = await stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: elements.getElement(CardElement),
-                billing_details: {
-                    name: me.name,
+    useEffect(() => {
+        if (stripe) {
+            const pr = stripe.paymentRequest({
+                country: 'US',
+                currency: 'usd',
+                total: {
+                    label: 'Demo total',
+                    amount: 1099,
                 },
-            }
-        });
-
-        if (result.error) {
-            // Show error to your customer (e.g., insufficient funds)
-            console.log(result.error.message);
-        } else {
-            // The payment has been processed!
-            if (result.paymentIntent.status === 'succeeded') {
-                console.log(result)
-                // Show a success message to your customer
-                // There's a risk of the customer closing the window before callback
-                // execution. Set up a webhook or plugin to listen for the
-                // payment_intent.succeeded event that handles any business critical
-                // post-payment actions.
-            }
+                requestPayerName: true,
+                requestPayerEmail: true,
+            });
+            pr.canMakePayment().then(result => {
+                if (result) {
+                    setPaymentRequest(pr);
+                }
+            });
         }
-    };
+    }, [stripe]);
 
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
+    //     if (!stripe || !elements) return;
+    //
+    //     const response = await intentPayment();
+    //     console.log(response)
+    //     const { clientSecret } = response.data.intentTourPayment;
+    //
+    //     const result = await stripe.confirmCardPayment(clientSecret, {
+    //         payment_method: {
+    //             card: elements.getElement(CardElement),
+    //             billing_details: {
+    //                 name: me.name,
+    //             },
+    //         }
+    //     });
+    //
+    //     if (result.error) {
+    //         // Show error to your customer (e.g., insufficient funds)
+    //         console.log(result.error.message);
+    //     } else {
+    //         // The payment has been processed!
+    //         if (result.paymentIntent.status === 'succeeded') {
+    //             console.log(result)
+    //             // Show a success message to your customer
+    //             // There's a risk of the customer closing the window before callback
+    //             // execution. Set up a webhook or plugin to listen for the
+    //             // payment_intent.succeeded event that handles any business critical
+    //             // post-payment actions.
+    //         }
+    //     }
+    // };
 
+    if (paymentRequest) {
+        return <PaymentRequestButtonElement options={{paymentRequest}} />
+    }
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={() => {}}>
             <div className="heading">
                 <h2 className="entoursPay">Entours Pay</h2>
-                <h3 className="name">{me.name}</h3>
+                {/*<h3 className="name">{me.name}</h3>*/}
             </div>
             <div className="card">
                 <div className="cardDetails">
-                    <CardElement options={CARD_ELEMENT_OPTIONS} />
+                    {paymentRequest && <PaymentRequestButtonElement options={{paymentRequest}} />}
+                    {/*<CardElement options={CARD_ELEMENT_OPTIONS} />*/}
                 </div>
             </div>
             <div className={classes.payButton}>
                 <StyledButton disabled={!stripe}>
-                    <img hidden={false} src={locker} className={classes.payIcon}  alt="secure"/>
-                    <span hidden={false}>Pay</span>
+                    <div style={{opacity: stripe ? '1' : '0'}}>
+                        <img hidden={false} src={locker} className={classes.payIcon}  alt="secure"/>
+                        <span hidden={false}>Pay</span>
+                    </div>
                     {!stripe && <ButtonLoading />}
                 </StyledButton>
             </div>
